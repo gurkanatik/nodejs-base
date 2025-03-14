4bbaa60848b5d704c27a5e0cac1d3eb285263917
gurkanatik <gurkanatik35@gmail.com>
2025-03-15 01:30:35 +0300

    feat: add sequelize support & db config

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ import routes from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+import sequelize from "./config/db.js";
+
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware.js";
 import { errorLoggerMiddleware } from "./middlewares/errorLoggerMiddleware.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+sequelize
+  .sync()
+  .then(() => console.log("✅ Database synced"))
+  .catch((err) => console.error("❌ Sync error:", err));
 
 app.set("view engine", "ejs");
 app.use("/", routes);
