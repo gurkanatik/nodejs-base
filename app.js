d6df9b7638d46e5b9030ee6ed7d5a38acf482db6
gurkanatik <gurkanatik35@gmail.com>
2025-03-15 00:37:49 +0300

    feat: setup controllers & routes & views structure

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import routes from "./routes/index.js";
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
