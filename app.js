4738b414fd465a60e5b8750f8489ee86e6152de2
gurkanatik <gurkanatik35@gmail.com>
2025-03-15 01:12:30 +0300

    chore: use app PORT from .env

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from "express";
 import routes from "./routes/index.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware.js";
 import { errorLoggerMiddleware } from "./middlewares/errorLoggerMiddleware.js";
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use("/", routes);
 app.use(notFoundMiddleware);
 app.use(errorLoggerMiddleware);
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
